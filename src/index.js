50b12fef9c24396f06f7770a6f9f8b3b275bf2c8
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-01-19 22:53:00 -0800

    Setting up Project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
 
+import logger  from 'redux-logger';
+import thunk from 'redux-thunk'
+
+
+
+import reducers from './reducers}';
 import App from './App';
 import './index.css';
 
+const store = createStore(reducers, applyMiddleware(thunk, logger));
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/reducers.js
@@ -0,0 +1,7 @@
+export const reducer = (state=initialState, action) => {
+    switch (action.type) {
+
+        default: 
+        return state;
+    }
+};
